d77f20e0a8767c3e4f39d9a7c67de83098428c9a
zhou zhenhua <citysir@qq.com>
2014-12-11 11:23:16 +0800

    https://github.com/citysir/fundc

--- web/btcoin/views.py
+++ web/btcoin/views.py
@@ -7,11 +7,15 @@ from fundc import config
 from fundc.models.btcoin import CnBtCoinMinutelyPrice
 
 def btchart(request):
-    days = request.REQUEST.get("days")
-    days = int(days) if days else 1  
-    limit = 60 * 24 * days
+    hours = request.REQUEST.get("h")
+    refresh = int(request.REQUEST.get("r", '0'))
+
+    hours = int(hours) if hours else 24  
+    limit = 60 * hours
     prices = list(CnBtCoinMinutelyPrice.objects.all().order_by('-id')[:limit])
     prices.reverse()
+
+    hours_desc = _get_hours_desc(hours)
      
     xAxis = [] #["1", "2", "3", "4", "5", ...]
     series = [] #  [ 2312, 2313, ... ]
@@ -24,3 +28,21 @@ def btchart(request):
     series = json.dumps(series)
     
     return render_to_response('btcoin/btchart.html', locals())
+
+
+def _get_hours_desc(hours):
+    if hours == 1:
+        return '1小时内'
+    elif hours == 6:
+        return '6小时内'
+    elif hours == 12:
+        return '12小时内'
+    elif hours == 24:
+        return '1天内'
+    elif hours == 72:
+        return '3天内'
+    elif hours == 168:
+        return '7天内'
+    else:
+        raise Exception("不支持的 hours %s" % hours)
+    
